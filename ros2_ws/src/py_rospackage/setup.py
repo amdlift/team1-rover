9f40e9519370ae5e60b9b19798a04cfdd9fb9f68
Aaron Davis <aaron.m.davis@comcast.net>
2025-10-25 22:16:33 -0500

    fixed package names

--- ros2_ws/src/py_rospackage/setup.py
+++ ros2_ws/src/py_rospackage/setup.py
@@ -24,8 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'talker = py_pubsub.publisher_member_function:main',
-            'listener = py_pubsub.subscriber_member_function:main',
+            'talker = py_rospackage.publisher_member_function:main',
+            'listener = py_rospackage.subscriber_member_function:main',
         ],
     },
 )
